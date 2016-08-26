0553261d5eb0648be731a8a842e21208ef105ea0
IsakFalk <isak.falk@live.se>
2016-08-26 15:22:28 +0100

    Add case for array in history object

--- src/history-generator.js
+++ src/history-generator.js
@@ -21,46 +21,75 @@ var compareJson = function(obj, history, commit) {
 
   for (var propertyName in obj) {
   	var historyProperty = '$hist_' + propertyName
+		var arrayProperty = '$hist_array' + propertyName
 
-		// base case: if we are at leaf
-    if (typeof(obj[propertyName]) != 'object') {
-      // simple type
-    	if (!history[propertyName]) {
-		  	history[historyProperty] =  "Created by " + commit.author + " at " + commit.date
-		  	history[propertyName] = obj[propertyName]
-		  	modified = true
-		  } else if (typeof(obj[propertyName]) != typeof(history[propertyName])) {
-		  	history[historyProperty] =  "Modified by " + commit.author + " at " + commit.date + ', Type changed'
+		// split into cases: if array
+		if (Array.isArray(obj[propertyName])){
+			arr_curr = obj[propertyName]
+			arr_hist = history[propertyName]
+
+			l = arr_curr.length
+			k = 0
+
+
+	    if (arr_curr.length !== arr_hist.length || !Array.isArray(arr_hist)) {
+				arrayProperty = "Updated by " + commit.author + " at " + commit.date
 				history[propertyName] = obj[propertyName]
-		  	modified = true
-			} else if (obj[propertyName] != history[propertyName]) {
-	  		history[historyProperty] =  "Modified by " + commit.author + " at " + commit.date
-	  		history[propertyName] = obj[propertyName]
-	  		modified = true
-	  	}
-		// else we check further down the tree
+				modified = true
+			}
+
+			// since the list might be jumbled, we loop over both, and check if they have the same elements
+			for (var index_curr in arr_curr) {
+				for (var index_hist in arr_hist) {
+					if (arr_curr[index_curr] === arr_hist[index_hist]) {
+						k += 1
+					}
+				}
+			}
+
+			if
+		// else in first case
 		} else {
-			// obj is not at leaf, history is at leaf
-		  if (!history[propertyName] || typeof(history[propertyName]) !== 'object') {
-				history[historyProperty] =  "Created by " + commit.author + " at " + commit.date
-				var childHistory = {}
-		  	var childModified = compareJson(obj[propertyName], childHistory, commit)
-		  	if (childModified) {
-		  		history[propertyName] = childHistory
-		  	}
-
-		  	modified = true
-				continue
-			// obj is not at leaf, history is not at leaf
-		  } else {
-		  	var childModified = compareJson(obj[propertyName], history[propertyName], commit)
-		  	modified = childModified
-		  	if (childModified) {
+		// base case: if we are at leaf
+	    if (typeof(obj[propertyName]) != 'object') {
+	      // simple type
+	    	if (!history[propertyName]) {
+			  	history[historyProperty] =  "Created by " + commit.author + " at " + commit.date
+			  	history[propertyName] = obj[propertyName]
+			  	modified = true
+			  } else if (typeof(obj[propertyName]) != typeof(history[propertyName])) {
+			  	history[historyProperty] =  "Modified by " + commit.author + " at " + commit.date + ', Type changed'
+					history[propertyName] = obj[propertyName]
+			  	modified = true
+				} else if (obj[propertyName] != history[propertyName]) {
 		  		history[historyProperty] =  "Modified by " + commit.author + " at " + commit.date
+		  		history[propertyName] = obj[propertyName]
+		  		modified = true
 		  	}
+			// else we check further down the tree
+			} else {
+				// obj is not at leaf, history is at leaf
+			  if (!history[propertyName] || typeof(history[propertyName]) !== 'object') {
+					history[historyProperty] =  "Created by " + commit.author + " at " + commit.date
+					var childHistory = {}
+			  	var childModified = compareJson(obj[propertyName], childHistory, commit)
+			  	if (childModified) {
+			  		history[propertyName] = childHistory
+			  	}
 
-		  	continue
-		  }
+			  	modified = true
+					continue
+				// obj is not at leaf, history is not at leaf
+			  } else {
+			  	var childModified = compareJson(obj[propertyName], history[propertyName], commit)
+			  	modified = childModified
+			  	if (childModified) {
+			  		history[historyProperty] =  "Modified by " + commit.author + " at " + commit.date
+			  	}
+
+			  	continue
+			  }
+			}
 		}
 	}
 	return modified
