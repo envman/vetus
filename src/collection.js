ddd0919e332c1b207654d45a78dbee0714e7087a
James Vickery <dev@jamesvickery.net>
2019-07-22 11:43:15 +0100

    rm unneeded precommands

--- src/collection.js
+++ src/collection.js
@@ -329,32 +329,28 @@ module.exports = function(options) {
   }
 
   const getVersion = callback => {
-    preCommand(() => {
-      barerepo.getLatestTag(branch, tag => {
-        if (!tag) return callback(undefined)
+    barerepo.getLatestTag(branch, tag => {
+      if (!tag) return callback(undefined)
 
-        const v = tag.match(/v_(.*)_/)
-        if (!v || !v.length) return callback(undefined)
+      const v = tag.match(/v_(.*)_/)
+      if (!v || !v.length) return callback(undefined)
 
-        const [major, minor] = v[1].split(/\./).map(Number)
-        callback({ major, minor })
-      })
+      const [major, minor] = v[1].split(/\./).map(Number)
+      callback({ major, minor })
     })
   }
 
   const allVersions = callback => {
-    preCommand(() => {
-      barerepo.getTags('v_*_', tags => {
-        if (!tags) return callback(undefined)
+    barerepo.getTags('v_*_', tags => {
+      if (!tags) return callback(undefined)
 
-        const versions = tags
-          .map(t => t.match(/v_(.*)_/))
-          .filter(t => t)
-          .map(t => t[1].split(/\./).map(Number))
-          .map(([major, minor]) => ({major, minor}))
+      const versions = tags
+        .map(t => t.match(/v_(.*)_/))
+        .filter(t => t)
+        .map(t => t[1].split(/\./).map(Number))
+        .map(([major, minor]) => ({major, minor}))
 
-        return callback(versions)
-      })
+      return callback(versions)
     })
   }
 
@@ -365,9 +361,11 @@ module.exports = function(options) {
     }
 
     const newTag = `v_${major}.${minor}_`
-    repo.preTagCommit(branch, newTag, targetCommit => {
-      barerepo.tag(newTag, targetCommit, ok => {
-        return callback(ok && version)
+    preCommand(() => {
+      repo.preTagCommit(branch, newTag, targetCommit => {
+        barerepo.tag(newTag, targetCommit, ok => {
+          return callback(ok && version)
+        })
       })
     })
   }
